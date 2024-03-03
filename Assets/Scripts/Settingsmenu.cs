3242de9adc92baef38d9156ced3bf68d98b593fa
531428aj <531428@escg.ac.uk>
2024-03-03 22:55:27 +0000

    Update

--- Assets/Scripts/Settingsmenu.cs
+++ Assets/Scripts/Settingsmenu.cs
@@ -2,13 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
+
 
 public class Settingsmenu : MonoBehaviour
 {
+    public Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+        //Clears the current drop down menu 
+        resolutionDropdown.ClearOptions();
+        // This starts a new list of options 
+        List<string> options = new List<string>();
+        //This finds the current resolution and shows it on the drop down
+        int currentResolutionIndex = 0;
+        //This takes every resolution it can find
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
     public AudioMixer audioMixer;
     public void SetVolume(float volume)
     {
         audioMixer.SetFloat("Master volume", volume);
     }
-
+    public void SetResolution (int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
 }
